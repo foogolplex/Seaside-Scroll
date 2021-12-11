0ce268985a377188c46d452d221588687f08750e
Robert Wood <robertwood@tuta.io>
2021-12-11 16:24:56 -0600

    Added story

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FOG from 'vanta/dist/vanta.fog.min'
 import {useEffect, useState, useRef} from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
+import { ReactReader } from 'react-reader';
 
 function App() {
   const [vantaEffect, setVantaEffect] = useState(0)
   const myRef = useRef(null)
+  const [location, setLocation] = useState(null);
+  const locationChanged = (epubcifi) => {
+    // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
+    setLocation(epubcifi)
+  }
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(FOG({
@@ -34,10 +41,25 @@ function App() {
       <header className="App-header">
         <h1 className="display-1">Seaside Scroll</h1>
         <h3 className="display-5">By Alex Kirzhnev</h3>
-         
-        <Button href="story" size="lg" style={{marginTop: "20px"}} variant="outline-light">Begin</Button>  
+        
+        <Link
+    activeClass="active"
+    to="start"
+    spy={true}
+    smooth={true}
+    offset={0}
+    duration={10}
+        >
+        <Button size="lg" style={{marginTop: "20px"}} variant="outline-light">Begin</Button> 
+        </Link>
       </header>
-      
+      <div style={{ height: "100vh" }} className="start">
+      <ReactReader
+        location={location}
+        locationChanged={locationChanged}
+        url="/SeasideScroll.epub"
+      />
+    </div>
     </div>
   );
 }
