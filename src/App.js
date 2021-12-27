3f4193f00e9531f45ef6acea73a1ae08a635e8d0
Robert Wood <robertwood@tuta.io>
2021-12-27 02:27:55 -0600

    Progress in solving book options

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FOG from 'vanta/dist/vanta.fog.min'
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState, useRef, ReactDOM} from 'react';
 // eslint-disable-next-line
 import { Link, animateScroll as scroll } from "react-scroll";
 import { ReactReader, ReactReaderStyle } from 'react-reader';
+import { findDOMNode, unmountComponentAtNode } from 'react-dom';
 
 let ownStyles = {
   ...ReactReaderStyle,
@@ -20,30 +21,101 @@ let ownStyles = {
   }*/
 }
 
-//replace option x with corresponding link
-//when clicking the link switch the epub with corresponding epub
-//when clicking return go back to the old epub
+// If page is the last one and the selected file is the 0th then render 3 buttons x
+// Each button has an onclick function that changes the selected file state x
+// If the file state changes then UseEffect will detect this x
+// Editing the currentfile state which should rerender the Reactreader component
+// I also need to report the location of the epubcifi at the last page and save it to a variable
+// if the page is the last one and the selected file isn't the 0th then render a button to change back to base state
+// if changing back to the base state then somehow rendition.display(old reported location)
 
-//add an empty page to the epub file
-//when I get to that empty page detect it
-//draw buttons corresponding to the options
-//when clicking the buttons render a new epub
-//corresponding to the new updated story 
 
-// cases: main story, option 1, option 2, option 3, return to options
-// find option 1, 2, text and replace with link tag
-// if you click the link then change state to corresponding case
-// depending on the case render a new react reader which is a modified epub of the altered story
-// if "return to story" is found replace it with link and if clicked then
-// go back to the main story
+const filenames = ['/SeasideScroll.epub', '/SeasideScroll1.epub', '/SeasideScroll2.epub', '/SeasideScroll3.epub']
 
+function MainReader(props){
+  
+  if(props.selectedfile === 0){
+    return (
+      <ReactReader
+        location={props.location}
+        locationChanged={props.locationchanged}
+        url={filenames[0]} 
+        showToc={false}
+        styles={ownStyles}
+        getRendition={(rendition) => {  
+            props.renditionref.current = rendition
+        }}
+        ref={props.readerRef}
+      ></ReactReader>
+    );
+  }
+  else if(props.selectedfile === 1){
+    return (
+      null, <ReactReader
+        location={props.location}
+        locationChanged={props.locationchanged}
+        url={filenames[1]} 
+        showToc={false}
+        styles={ownStyles}
+        getRendition={(rendition) => {  
+            props.renditionref.current = rendition
+        }}
+        ref={props.readerRef}
+      ></ReactReader>
+    );
+  }
+  else if(props.selectedfile === 2){
+    /*return (
+      <ReactReader
+        location={props.location}
+        locationChanged={props.locationchanged}
+        url={filenames[2]} 
+        showToc={false}
+        styles={ownStyles}
+        getRendition={(rendition) => {  
+            props.renditionref.current = rendition
+        }}
+        ref={props.readerRef}
+      ></ReactReader>
+      
+    );*/
+  }
+  else if(props.selectedfile === 3){
+    return (
+      <ReactReader
+        location={props.location}
+        locationChanged={props.locationchanged}
+        url={filenames[3]} 
+        showToc={false}
+        styles={ownStyles}
+        getRendition={(rendition) => {  
+            props.renditionref.current = rendition
+        }}
+        ref={props.readerRef}
+      ></ReactReader>
+    );
+  }
+  return (null);
+}
 
 function App() {
   const [page, setPage] = useState('') 
   const [mode, setMode] = useState('Dark')
-  const [buttonMode, setButtonMode] = useState('dark') 
-  var changeMode = () => {
-    console.log(mode);
+  const [buttonMode, setButtonMode] = useState('dark')
+  const [selectedFile, setSelectedFile] = useState(0);
+  const [filename, setFileName] = useState(filenames[selectedFile]);
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+  const [location, setLocation] = useState(null);
+  const [size, setSize] = useState(100)
+  const renditionRef = useRef(null)
+  const [total, setTotal] = useState(null)
+  const readerRef = useRef(null)
+  const [tempState, setTempState] = useState(0);
+  const changeSize = (newSize) => {
+    setSize(newSize)
+  } 
+  const changeMode = () => {
     if(mode === 'Light' && renditionRef.current){
       ownStyles.readerArea.backgroundColor = 'white'
       //ownStyles.tocBackground.background = 'white'
@@ -78,21 +150,21 @@ function App() {
       setButtonMode('light'); 
       setMode('Light');
     }
-    console.log(mode);
   }
-  
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
-  const [location, setLocation] = useState(null);
   const locationChanged = (epubcifi) => {
     // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
     setLocation(epubcifi)
     // eslint-disable-next-line
-    const { displayed, href } = renditionRef.current.location.start
-    
-    setPage(`${displayed.page} of ${displayed.total}`)
+    if(renditionRef.current){
+      const { displayed, href } = renditionRef.current.location.start
+      setTotal(displayed.total)
+      setPage(`${displayed.page} of ${displayed.total}`)
+    }
     
   }
+  useEffect(() => {
+    setFileName(filenames[selectedFile]) 
+  }, [selectedFile])
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(FOG({
@@ -115,11 +187,6 @@ function App() {
       if (vantaEffect) vantaEffect.destroy()
     }
   }, [vantaEffect]) 
-  const [size, setSize] = useState(100)
-  const renditionRef = useRef(null)
-  const changeSize = (newSize) => {
-    setSize(newSize)
-  } 
   useEffect(() => {
     if (renditionRef.current) {
       renditionRef.current.themes.default({ 
@@ -145,22 +212,21 @@ function App() {
         </Link>
       </header>
       <div style={{ height: "100vh", backgroundColor: "black", background: "black"}} className="start">
-        <ReactReader
-          location={location}
-          locationChanged={locationChanged}
-          url="/SeasideScroll.epub" 
-          showToc={true}
-          styles={ownStyles}
-          getRendition={(rendition) => {  
-              renditionRef.current = rendition  
-          }}
-        />
+        
+         
+        <MainReader settempstate={setTempState} selectedfile={selectedFile} location={location} locationchanged={locationChanged} renditionref={renditionRef} readerref={readerRef} tempstate={tempState}/>
+        
+        
       </div>
       <div style={{ position: 'absolute', bottom: '1rem', right: '1rem', left: '1rem', textAlign: 'left', zIndex: 1}}>
           <Button variant={"outline-"+buttonMode} onClick={() => changeSize(Math.max(90, size - 10))}>-</Button> 
-          <Button variant={"outline-"+buttonMode} onClick={() => changeSize(Math.min(130, size + 10))}>+</Button> 
+          <Button variant={"outline-"+buttonMode} onClick={() => changeSize(Math.min(130, size + 10))}>+</Button>
+          {(parseInt(page) === parseInt(total) || Math.abs(parseInt(page) - parseInt(total)) <= 2) ? 
+          <span><Button variant={"outline-"+buttonMode} onClick={() => setSelectedFile(1) }>1</Button>
+          <Button variant={"outline-"+buttonMode} onClick={() => setSelectedFile(2) }>2</Button>
+          <Button variant={"outline-"+buttonMode} onClick={() => setSelectedFile(3) && setTempState(0) }>3</Button></span>: null} 
       </div>
-      <div style={{ position: 'absolute', top: '104vh', right: '1rem', left: '1rem', textAlign: 'right', zIndex: 2}}>
+      <div style={{ position: 'absolute', top: '101vh', right: '1rem', left: '1rem', textAlign: 'right', zIndex: 2}}>
           <Button variant={"outline-"+buttonMode} onClick={() => changeMode()}>{mode === 'Dark' ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
             <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
             </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
@@ -168,7 +234,7 @@ function App() {
             </svg>}
           </Button>
       </div>
-      <div style={{ position: 'absolute', top: '104vh', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1}}>
+      <div style={{ position: 'absolute', top: '101vh', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1}}>
           <span style={mode === 'Dark' ? {color: 'black'} : {color: 'white'}}>{page}</span> 
       </div>
       <div style={{ position: 'absolute', top: '1rem', right: '1rem', left: '1rem', textAlign: 'center', zIndex: 1}}>
